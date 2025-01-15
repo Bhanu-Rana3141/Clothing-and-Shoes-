feaceac8119d56ceb31b7aca6d3b9c5db43c7ecd
Bhanu Partap Singh Rana <bhanurana3826@gmail.com>
2025-01-16 02:39:07 +0530

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav>
-      <img height={'60px'} src="Images\1_Logo.png" alt="" />
+      <img height={'40px'} src="Images\logo.png" alt="" />
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/shoes">Shoes</Link></li>
